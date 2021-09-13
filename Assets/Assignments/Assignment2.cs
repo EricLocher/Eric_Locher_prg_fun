279dfe64472ec8e0f7a40dbff0db3ab893dba913
EricLocher <eriloc1129@skola.goteborg.se>
2021-09-13 10:54:10 +0100

    Done the assignment.
    Done the second assignment and started working on the extracurricular.

---
+++ Assets/Assignments/Assignment2.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment2 : ProcessingLite.GP21
+{
+    float spaceBetweenLines = 0.2f;
+    ParabolicCurve test, test2;
+    void Start()
+    {
+        Stroke(129, 128, 128);
+        test = new ParabolicCurve(100, 100, 200);
+        test2 = new ParabolicCurve(-100, -100, 200);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        test.draw();
+        test2.draw();
+
+        /*for (int i = 0; i < 10; i++)
+        {
+            if((i % 3) == 0) {
+                Stroke(0, 0, 0);
+            } else
+            {
+                Stroke(129, 128, 128);
+            }
+
+            Debug.Log(i % 3);
+            Line(0, (10 - i), (0 + i), 0);
+
+        }*/
+
+    }
+}
+
+
+class ParabolicCurve : ProcessingLite.GP21 {
+    float axis1, axis2;
+    int numberOfLines;
+
+
+    public ParabolicCurve(float axis1, float axis2, int numberOfLines)
+    {
+        this.axis1 = axis1;
+        this.axis2 = axis2;
+        this.numberOfLines = numberOfLines;
+    }
+
+    public void draw()
+    {
+        for (int i = 0; i < numberOfLines; i++)
+        {
+           
+           float x = i * (axis2 / numberOfLines);
+           float y = axis2 - (i * (axis1 / numberOfLines));
+
+           Line(x, 0, 0, y);
+        }
+    }
+
+}
