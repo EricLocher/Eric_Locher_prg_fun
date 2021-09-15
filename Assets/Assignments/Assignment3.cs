b6cb33e61cae57ee76efc3845e2369be2602f2ed
EricLocher <eriloc1129@skola.goteborg.se>
2021-09-15 16:11:15 +0200

    Liten korrektion

--- Assets/Assignments/Assignment3.cs
+++ Assets/Assignments/Assignment3.cs
@@ -44,16 +44,16 @@ public class Circle : ProcessingLite.GP21
 
     //Cirkelns Position x,y. Vector2 beskriver ett objekten som håller i ett x och y värde.
     public Vector2 pos; 
-    //Cirkelns radius
-    public float radius;
+    //Cirkelns diameter
+    public float diameter;
     //Cirkelns Velocitet i x,y led. I detta fallet beskriver då Vector2 hur snabbt cirkeln färdas i x,y led.
     public Vector2 velocity = new Vector2(0, 0);
 
-    //Cirkelns Contructor, passerar in en "Startposition" för cirkeln samt ett radius värde.
-    public Circle(Vector2 pos, float radius = 1)
+    //Cirkelns Contructor, passerar in en "Startposition" för cirkeln samt ett diameter värde.
+    public Circle(Vector2 pos, float diameter = 1)
     {
         this.pos = pos;
-        this.radius = radius;
+        this.diameter = diameter;
     }
 
     //Rit funktion som ska kallas varje frame.
@@ -65,19 +65,19 @@ public class Circle : ProcessingLite.GP21
         pos.y += Time.deltaTime * velocity.y;
 
         //Kollar om cirkeln är utanför spelytan, om den är det så *-1 för att invertera riktningen
-        if (pos.x > Width || pos.x < 0)
+        if ((pos.x + diameter / 2) > Width || (pos.x - diameter / 2) < 0)
         {
             velocity.x *= -1;
         }
 
         //Kollar om cirkeln är utanför spelytan, om den är det så *-1 för att invertera riktningen
-        if (pos.y > Height || pos.y < 0)
+        if ((pos.y + diameter / 2) > Height || (pos.y - diameter / 2) < 0)
         {
             velocity.y *= -1;
         }
 
         //Rita cirkeln
-        Circle(pos.x, pos.y, 1f);
+        Circle(pos.x, pos.y, diameter);
     }
 
     public void move(float speed, Vector2 direction)
