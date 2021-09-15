4b53610d929c47c47b651787765821a5410ce325
EricLocher <eriloc1129@skola.goteborg.se>
2021-09-15 11:47:23 +0200

    Update Assignment2.cs

--- Assets/Assignments/Assignment2.cs
+++ Assets/Assignments/Assignment2.cs
@@ -1,25 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Assignment2 : ProcessingLite.GP21
 {
-    float spaceBetweenLines = 0.2f;
-    ParabolicCurve test, test2;
-    void Start()
+    private float spaceBetweenLines = 0.2f;
+    private ParabolicCurve test, test2;
+
+    private void Start()
     {
         Stroke(129, 128, 128);
-        test = new ParabolicCurve(100, 100, 200);
-        test2 = new ParabolicCurve(-100, -100, 200);
+        test = new ParabolicCurve(-500, 500, 200);
+        //test2 = new ParabolicCurve(-100, -100, 200);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
         test.draw();
-        test2.draw();
+        //test2.draw();
 
+
+       
+
+        
         /*for (int i = 0; i < 10; i++)
         {
             if((i % 3) == 0) {
@@ -31,17 +31,20 @@ public class Assignment2 : ProcessingLite.GP21
 
             Debug.Log(i % 3);
             Line(0, (10 - i), (0 + i), 0);
-
         }*/
-
     }
+
+
+    /**
+     * Uppgift 4 beskriver en function som "tvingar" ett värde att vara innuti ett givit intervall.
+     * Denna funktionen är då bra i tillfällen där man vill att värden ligger inom ett visst intervall. 
+     */
 }
 
-
-class ParabolicCurve : ProcessingLite.GP21 {
-    float axis1, axis2;
-    int numberOfLines;
-
+public class ParabolicCurve : ProcessingLite.GP21
+{
+    private float axis1, axis2;
+    private int numberOfLines;
 
     public ParabolicCurve(float axis1, float axis2, int numberOfLines)
     {
@@ -54,20 +57,10 @@ class ParabolicCurve : ProcessingLite.GP21 {
     {
         for (int i = 0; i < numberOfLines; i++)
         {
+            float x = i * (axis2 / numberOfLines);
+            float y = axis2 - (i * (axis1 / numberOfLines));
 
-            if(i % 3 == 0)
-            {
-                Stroke(0, 0, 0);
-            } else
-            {
-                Stroke(128, 128, 128);
-            }
-           
-           float x = i * (axis2 / numberOfLines);
-           float y = axis2 - (i * (axis1 / numberOfLines));
-
-           Line(x, 0, 0, y);
+            Line(x, 0, 0, y);
         }
     }
-
 }
