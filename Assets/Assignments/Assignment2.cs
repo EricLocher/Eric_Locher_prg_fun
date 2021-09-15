e0352abd261bb00efa54b00aae575f479ef8c43e
EricLocher <eriloc1129@skola.goteborg.se>
2021-09-15 13:22:03 +0200

    Tog bort oanvänd kod

--- Assets/Assignments/Assignment2.cs
+++ Assets/Assignments/Assignment2.cs
@@ -1,43 +1,21 @@
 public class Assignment2 : ProcessingLite.GP21
 {
-    private float spaceBetweenLines = 0.2f;
-    private ParabolicCurve test, test2;
+    private ParabolicCurve Curve;
 
-    private void Start()
+    void Start()
     {
         Stroke(129, 128, 128);
-        test = new ParabolicCurve(-500, 500, 200);
-        //test2 = new ParabolicCurve(-100, -100, 200);
+        Curve = new ParabolicCurve(500, 500, 200);
     }
 
-    // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        test.draw();
-        //test2.draw();
-
-
-       
-
-        
-        /*for (int i = 0; i < 10; i++)
-        {
-            if((i % 3) == 0) {
-                Stroke(0, 0, 0);
-            } else
-            {
-                Stroke(129, 128, 128);
-            }
-
-            Debug.Log(i % 3);
-            Line(0, (10 - i), (0 + i), 0);
-        }*/
+        Curve.draw();
     }
-
 
     /**
-     * Uppgift 4 beskriver en function som "tvingar" ett värde att vara innuti ett givit intervall.
-     * Denna funktionen är då bra i tillfällen där man vill att värden ligger inom ett visst intervall. 
+     * Uppgift 4 beskriver en function som "tvingar" ett värde att vara innuti ett givet intervall.
+     * Denna funktionen är då bra i tillfällen där man vill att värden ligger inom ett visst intervall.
      */
 }
 
