d47b527a4f6dbd03f9417bc6a78b28aa95ee68a4
EricLocher <eriloc1129@skola.goteborg.se>
2021-09-17 15:43:11 +0200

    Fixade acc

--- Assets/Assignments/Assignment4.cs
+++ Assets/Assignments/Assignment4.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 
 public class Assignment4 : ProcessingLite.GP21
 {
-    float speed = 3f;
-    float acc;
     Character gubbe;
-    Vector2 keyDown = new Vector2(0, 0);
-
-
     // Start is called before the first frame update
     private void Start()
     {
-        gubbe = new Character(new Vector2(3,3));
+        gubbe = new Character(new Vector2(3,3), 5f);
     }
 
     // Update is called once per frame
@@ -29,26 +24,28 @@ public class Character : ProcessingLite.GP21
 {
     public Vector2 pos;
     private Vector2 speed;
+    private float acc;
 
-    public Character(Vector2 pos)
+    public Character(Vector2 pos, float acc)
     {
         this.pos = pos;
+        this.acc = acc;
     }
 
     public void draw()
     {
+
         if (Input.GetAxisRaw("Horizontal") != 0)
         {
-            speed.x += (2f * Input.GetAxisRaw("Horizontal")) * Time.deltaTime;
+            speed.x += (acc * Input.GetAxisRaw("Horizontal")) * Time.deltaTime;
             pos.x += Time.deltaTime * speed.x;
         }
-
         else
         {
             if(speed.x > 0)
-                speed.x -= 2f * Time.deltaTime;
+                speed.x -= acc * Time.deltaTime;
             else if(speed.x < 0)
-                speed.x += 2f * Time.deltaTime;
+                speed.x += acc * Time.deltaTime;
 
             pos.x += Time.deltaTime * speed.x;     
         }
@@ -56,16 +53,15 @@ public class Character : ProcessingLite.GP21
 
         if(Input.GetAxisRaw("Vertical") != 0)
         {
-            speed.y += (2f * Input.GetAxisRaw("Vertical")) * Time.deltaTime;
+            speed.y += (acc * Input.GetAxisRaw("Vertical")) * Time.deltaTime;
             pos.y += Time.deltaTime * speed.y;
         }
-
         else
         {
             if (speed.y > 0)
-                speed.y -= 2f * Time.deltaTime;
+                speed.y -= acc * Time.deltaTime;
             else if (speed.y < 0)
-                speed.y += 2f * Time.deltaTime;
+                speed.y += acc * Time.deltaTime;
 
             pos.y += Time.deltaTime * speed.y;
         }
